2e3b3b6be36e9d30c60a0009038f2500b81301ed
xlzh502 <xlzh502@163.com>
2020-09-16 22:50:56 +0800

    再提交

--- Computer systems--A Programmers perspective/读书.cpp
+++ Computer systems--A Programmers perspective/读书.cpp
@@ -21,15 +21,49 @@ std::string show_bytes(byte_pointer buf, int cnt)
     return result;
 }
 
+void inplace_swap(int*  x, int *y)
+{
+    *x = *x ^ *y;
+    *y = *x ^ *y;
+    *x = *y ^ *x;
+}
+
+
 int main(int argc, char* argv[])
 {
     int i = 12345;
     assert(show_bytes((byte_pointer)&i, sizeof(i)) == "39300000");  // P79  Figure 2.6
 
     std::string str = "abcdef";
-    assert(show_bytes(str.c_str(), str.length()) == "616263646566");
+    assert(show_bytes(str.c_str(), str.length()) == "616263646566"); // P80  Practice Problem 2.7;    Text data is more platform-independent than binary data.
+
+    int j = 98765;
+    inplace_swap(&i, &j);
+    assert(i == 98765 && j == 12345);   // P85  Practice problem 2.10
+
+    /*
+   P89  Aside  C++编译器处理的逻辑 和 C编译器不太一样。 
+    int k = 0xfedcba98 << 32;
+    assert(0xfedcba98 << 32 == 0xfedcba98);
+    assert(0xfedcba98 >> 36 == 0xffedcba9);
+    assert(0xfedcba98U >> 40 == 0x00fedcba); */
+    
+    assert(1 << 2 + 3 << 4 == 1 << (2 + 3) << 4); // P90  Aside 运算符优先级的陷阱
 
 
+    //  P99 : conversion between signed and unsigned  ----   keep the bit values identical but change how these bits are interpreted.
+    //  P100:   General rule for how C handle conversion between signed and unsigned numbers with same word size ---- the numeric values might change , but the bit patterns do not.
+    short  v = -12345;
+    unsigned short uv = v;
+    assert(show_bytes((byte_pointer)&v, sizeof(short)) == show_bytes((byte_pointer)&uv, sizeof(unsigned short)));
 
+    int m = -1;
+    unsigned int n = UINT_MAX + m+1;  // P101:   公式（2.5）  给出一个负数（有符号数，且负数），求与它具有相同 位模式 的 无符号数。
+    assert(show_bytes((byte_pointer)&m, sizeof(int)) == show_bytes((byte_pointer)&n, sizeof(unsigned int)));
+
+    // P105:  Figure 2.18   几个违反常理的情况
+    assert(-1 < 0U == false);
+    assert(2147483637U > -2147483637 - 1 == false);
+    assert(2147483637 > (int)2147483638U == true);
 }
 

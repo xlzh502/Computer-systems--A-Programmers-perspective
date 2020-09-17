47af8a37e599fc311b4c0bdc3b9f415eb93d6024
xlzh502 <xlzh502@163.com>
2020-09-17 12:56:28 +0800

    check in

--- Computer systems--A Programmers perspective/读书.cpp
+++ Computer systems--A Programmers perspective/读书.cpp
@@ -29,6 +29,48 @@ void inplace_swap(int*  x, int *y)
 }
 
 
+// TEMPLATE CLASS is_unsigned
+
+/*template<class _Ty>
+struct is_unsigned
+    : std::_Sign_base<_Ty>::_Unsigned
+{	// determine whether _Ty is an unsigned type
+public:
+    is_unsigned(_Ty& val) {}
+};*/
+
+// 这里声明这个模板函数，而不是上面的模板类，就是为了利用如下特性：模板函数 可以根据 实参的类型，来 推断模板的参数； 但是，模板类却不行。 因此，上面的模板类被注释掉了。
+// 有关模板函数 的这一特性，参考《C++程序设计语言（特别版）》第13.3.1节
+template <typename T>
+bool is_unsigned(const T& t)
+{
+    return std::is_unsigned<T>::value;
+}
+
+// is_longlong 利用了 模板函数的重载、模板函数的专门化
+template <typename T>
+bool is_longlong(const T& t)
+{
+    return false;
+}
+
+template<>
+bool is_longlong(const long long& t)
+{
+    return true;
+}
+
+//本打算复制、粘贴个 is_int 模板函数，但是发现这样写个宏，更好些。
+#define IS_TYPE(TYP)  template <typename T>  bool is_##TYP(const T & t) {  return false ;} \
+                                                                            bool is_##TYP(const TYP& t) {   return true ;}
+
+IS_TYPE(int) // is_int 模板函数
+IS_TYPE(long)
+
+#define IS_TYPE(a,b,c)  template <typename T>  bool is_##a##b##c(const a b c & t) {  return false ;} \
+                                                                            bool is_##a##b##c(const a b c & t) {   return true ;}
+IS_TYPE(unsigned, long, long)
+
 int main(int argc, char* argv[])
 {
     int i = 12345;
@@ -62,8 +104,28 @@ int main(int argc, char* argv[])
     assert(show_bytes((byte_pointer)&m, sizeof(int)) == show_bytes((byte_pointer)&n, sizeof(unsigned int)));
 
     // P105:  Figure 2.18   几个违反常理的情况
+    //  编译的时候需要去掉SDL检查，否则，会报 “C4146：一元负运算符应用于无符号类型，结果仍为无符号类型” 错误。 
     assert(-1 < 0U == false);
-    assert(2147483637U > -2147483637 - 1 == false);
-    assert(2147483637 > (int)2147483638U == true);
+    assert(2147483647U > -2147483647 - 1 == false);
+    assert(2147483647 > (int)2147483648U == true);
+    assert(is_unsigned(0x80000000) == true);
+    assert(is_unsigned(-2147483647 - 1) == false);
+
+    if (sizeof(char*) == 8)
+    {
+        // 64位机器
+        assert(is_long(-2147483648) == true);
+        assert(is_int(-0x7FFFFFFF - 1) == true);
+    }
+    else if (sizeof(char*) == 4)
+    {
+         // 32位机器
+        assert(is_unsigned(-2147483648) == true);
+        assert(is_int(-0x7FFFFFFF - 1) == true);
+        assert(is_unsignedlonglong(-9223372036854775808) && is_unsigned(-9223372036854775808));
+        assert(is_unsignedlonglong(0x8000000000000000) && is_unsigned(0x8000000000000000 ));
+    }
+
+
 }
 
